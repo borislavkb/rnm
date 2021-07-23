286c58676970433e880f7621cd9839395e574420
Borislav Borislavov <borislavkb@gmail.com>
2021-07-23 23:45:41 +0200

    Add a logo on header, add style to image

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import "./Header.css";
 export default function Header() {
   return (
     <header className="App__header">
-      <img src="" alt="Logo" />
+      <img src="https://www.wallpapertip.com/wmimgs/1-11371_rick-and-morty-svg-free.jpg" />
       <nav className="App__navigation">
         <a href="/">Home</a>
         <a href="/characters">Characters</a>

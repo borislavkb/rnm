2bdc857ae78218b82779e50358386f5e258af4d7
Borislav Borislavov <borislavkb@gmail.com>
2021-07-24 00:13:30 +0200

    Add picture on Home, edit className for both picture on Home & on Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,11 @@ import "./Header.css";
 export default function Header() {
   return (
     <header className="App__header">
-      <img src="https://www.wallpapertip.com/wmimgs/1-11371_rick-and-morty-svg-free.jpg" />
+      <img
+        className="App__header____Logo"
+        src="https://www.wallpapertip.com/wmimgs/1-11371_rick-and-morty-svg-free.jpg"
+        alt="Rick and Morty"
+      />
       <nav className="App__navigation">
         <a href="/">Home</a>
         <a href="/characters">Characters</a>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,15 @@
 import "./Home.css";
+import img_cover from "../images/cover.png";
 
 export default function Home() {
   return (
     <section className="App__content____Home">
       <h1>Welcome to the Rick and Morty App!</h1>
-      <img src="" alt="" />
+      <img
+        className="App__content____Image"
+        src={img_cover}
+        alt="Rick and Morty falling into a void"
+      />
       <h2>Developed by Boris during the neuefische Bootcamp</h2>
     </section>
   );

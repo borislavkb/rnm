a40ee27e842af4008cc1a83f47a282bf25f595c0
Borislav Borislavov <borislavkb@gmail.com>
2021-07-24 08:19:24 +0200

    Add css style to characters section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import Home from "./components/Home";
 import Header from "./components/Header";
+import CharacterListPage from "./components/CharacterListPage";
+
 function App() {
   return (
     <div className="App">
       <Header />
       <main className="App__content">
         <Home />
+        <CharacterListPage />
       </main>
     </div>
   );

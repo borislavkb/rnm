f78b18348635804e81b275939e742820d6c77932
Borislav Borislavov <borislavkb@gmail.com>
2021-07-24 09:50:41 +0200

    Create SingleCharacterPage component and SingleCharacterPage.css

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,9 +1,10 @@
 import "./CharacterCard.css";
+import image from "../images/logo.png";
 
 export default function CharacterCard() {
   return (
     <li className="character__list____card">
-      <img src="" alt="" />
+      <img src={image} alt="" className="character__list____img" />
       <h3>Name</h3>
     </li>
   );
---
+++ src/components/SingleCharacterPage.js
@@ -0,0 +1,14 @@
+import "./SingleCharacterPage.css";
+
+export default function SingleCharacterPage() {
+  return (
+    <section className="Single__character">
+      <h2>Character Name</h2>
+      <img src="" alt="" />
+      <p>Status: </p>
+      <p>Gender: </p>
+      <p>Species: </p>
+      <p>Type: </p>
+    </section>
+  );
+}

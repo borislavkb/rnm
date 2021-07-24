d551537d64bda44443ed2e521cc3066b5db59658
Borislav Borislavov <borislavkb@gmail.com>
2021-07-24 10:24:04 +0200

    Edit specificity on Router on App.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import Home from "./pages/Home";
 import Header from "./components/Header";
 import CharacterListPage from "./pages/CharacterListPage";
 import SingleCharacterPage from "./pages/SingleCharacterPage";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <main className="App__content">
-        <Home />
-        <CharacterListPage />
-        <SingleCharacterPage />
+        <Switch>
+          <Route path="/characters/singleCharacterPage/:id">
+            <SingleCharacterPage />
+          </Route>
+          <Route path="/characters">
+            <CharacterListPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </main>
     </div>
   );

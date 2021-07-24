e626af0e11570f42b58aae05398ba50fb5bbf976
Borislav Borislavov <borislavkb@gmail.com>
2021-07-24 11:48:53 +0200

    Add API fetch on SingleCharacterPage

--- src/pages/SingleCharacterPage.js
+++ src/pages/SingleCharacterPage.js
@@ -1,14 +1,41 @@
 import "./SingleCharacterPage.css";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+//https://rickandmortyapi.com/api/character/:id
 
 export default function SingleCharacterPage() {
-  return (
-    <section className="Single__character">
-      <h2>Character Name</h2>
-      <img src="" alt="" />
-      <p>Status: </p>
-      <p>Gender: </p>
-      <p>Species: </p>
-      <p>Type: </p>
-    </section>
-  );
+  const { id } = useParams();
+  const [character, setCharacter] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const url = `https://rickandmortyapi.com/api/character/${id}`;
+    setIsLoading(true);
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCharacter(data);
+        setIsLoading(false);
+      });
+  }, [id]);
+
+  function renderCharacter() {
+    if (isLoading || character === null) {
+      return "Loading...";
+    }
+    const { name, gender, status, type, species, image } = character;
+
+    return (
+      <section className="Single__character">
+        <h2>{name}</h2>
+        <img src={image} alt={name} />
+        <p>Status: {status} </p>
+        <p>Gender: {gender}</p>
+        <p>Species: {species}</p>
+        <p>Type: {type} </p>
+      </section>
+    );
+  }
+  return renderCharacter();
 }

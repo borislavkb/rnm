d2b70747b0e9872656073d911ad393966407671c
Borislav Borislavov <borislavkb@gmail.com>
2021-07-24 08:13:35 +0200

    Add CharacterListPage basic html structure, add css file and import to Ap

---
+++ src/components/CharacterListPage.js
@@ -0,0 +1,14 @@
+import "./CharacterListPage.css";
+
+export default function CharacterListPage() {
+  return (
+    <section className="Characters">
+      <ul className="Character__list">
+        <li className="Character__list____card">
+          <img src="" alt="" />
+          <h3>Name</h3>
+        </li>
+      </ul>
+    </section>
+  );
+}

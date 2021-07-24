86407be9bb7fd0764abd1c52b034b7ae18862ba2
Borislav Borislavov <borislavkb@gmail.com>
2021-07-24 08:32:53 +0200

    Create CharacterCard component, import to CharacterListPage, add css file. Add Form component to CharacterListPage

---
+++ src/components/CharacterCard.js
@@ -0,0 +1,10 @@
+import "./CharacterCard.css";
+
+export default function CharacterCard() {
+  return (
+    <li className="character__list____card">
+      <img src="" alt="" />
+      <h3>Name</h3>
+    </li>
+  );
+}
--- src/components/CharacterListPage.js
+++ src/components/CharacterListPage.js
@@ -1,22 +1,13 @@
 import "./CharacterListPage.css";
+import CharacterCard from "./CharacterCard";
+import Form from "./Form";
 
 export default function CharacterListPage() {
   return (
     <section className="characters">
-      <form>
-        <input type="text" name="characterName" id="characterName" />
-        <selection className="characters__status">
-          <option>All</option>
-          <option>Unknown</option>
-          <option>Dead</option>
-          <option>Alive</option>
-        </selection>
-      </form>
+      <Form />
       <ul className="character__list">
-        <li className="character__list____card">
-          <img src="" alt="" />
-          <h3>Name</h3>
-        </li>
+        <CharacterCard />
       </ul>
     </section>
   );

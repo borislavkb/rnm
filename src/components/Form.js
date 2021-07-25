7ac1d2367434340b9ca7e6fda7cfc5ff32dc12de
Borislav Borislavov <borislavkb@gmail.com>
2021-07-25 22:15:45 +0200

    Add values to options of select on Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,10 +5,10 @@ export default function Form() {
     <form className="character__form">
       <input type="text" name="characterName" id="characterName" />
       <select className="characters__form____status">
-        <option>All</option>
-        <option>Unknown</option>
-        <option>Dead</option>
-        <option>Alive</option>
+        <option value="all">All</option>
+        <option value="unknown">Unknown</option>
+        <option value="dead">Dead</option>
+        <option value="alive">Alive</option>
       </select>
     </form>
   );
